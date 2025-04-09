4493b5583e48fd0b0d7939c472b0a1d1978fefee
vsbruna <141277861+vsbruna@users.noreply.github.com>
2025-04-09 08:30:52 -0300

    .

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-05-09T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
